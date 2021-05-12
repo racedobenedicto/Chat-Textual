1f51c7f6b34c7c6c86000004656213f807847619
racedobenedicto <74828495+racedobenedicto@users.noreply.github.com>
2021-05-12 15:34:34 +0200

    Add files via upload

---
+++ ClientThread.java
@@ -0,0 +1,106 @@
+import java.util.LinkedList;
+
+public class ClientThread implements Runnable {
+
+    private MySocket socket;
+    private String nick;
+    private ChatClient c;
+    private final LinkedList<String> messagesToSend;
+    private boolean hasMessages = false;
+
+    public ClientThread(ChatClient c) {
+        this.c = c;
+        this.socket = new MySocket(c.getHost(), c.getPort());
+        this.messagesToSend = new LinkedList<String>();
+    }
+
+    public void addNextMessage(String message) {
+        synchronized (messagesToSend) {
+            this.hasMessages = true;
+            this.messagesToSend.push(message);
+        }
+    }
+
+    @Override
+    public void run() {
+        /* TO KNOW WHICH PORT ARE WE CONNECTED TO
+         System.out.println("Local Port : " + mysocket.getLocalPort());
+         System.out.println("Servidor = " + mysocket.getRemoteSocketAddress() + ":" + mysocket.getPort());
+        */
+        try {
+            socket.flush();
+            Thread thInput=new Thread(){
+                public void run(){
+                    
+                    while (socket != null && !socket.isClosed()) {
+                        if (socket.ready()) {
+                            String line = socket.readLine();
+                            //socket.closeOut();
+                            if (line != null) {
+                                String[] message = line.split(":", 2);
+                                switch (message[0]) {
+                                    case "ACCEPT":
+                                        c.setState("JOIN");
+                                        break;
+                                    case "JOIN":
+                                        nick = message[1];
+                                        c.setNick(message[1]);
+                                        c.setState("MESSAGE");
+                                        break;
+                                    case "QUIT":
+                                        socket.println("QUIT:" + nick);
+                                        System.out.println("GoodBye");
+                                        return;
+                                    case "MESSAGE":
+                                        System.out.print(c.getColour(message[1].split(" ->")[0]));
+                                        System.out.println(message[1]);
+                                        System.out.print(Codes.WHITE);
+                                        break;
+                                    case "USERS":
+                                        c.updateUsers(message[1]);
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            thInput.start();
+            
+            Thread thOutput=new Thread(){
+                public void run(){
+                   // socket.closeIn();
+                    while (socket != null && !socket.isClosed()) {
+                        hasMessages = !messagesToSend.isEmpty();
+                        if (hasMessages) {
+                            String nextSend = "";
+                            synchronized (messagesToSend) {
+                                nextSend = messagesToSend.pop();
+                                if (nextSend.startsWith("QUIT:")) {
+                                    c.setState("QUIT");
+                                    //break;
+                                }
+                                hasMessages = !messagesToSend.isEmpty();
+                            }
+
+                            if (c.getState() == "ACCEPT" || c.getState() == "JOIN") {
+                                socket.println(nextSend);
+                            } else {
+                                System.out.print(Codes.ERASE_LINE_UP);
+                                socket.println(nick + " -> " + nextSend);
+                            }
+                            socket.flush();
+                        }
+                    }
+                }        
+            };
+            thOutput.start();
+            thOutput.join();
+            thInput.join();
+            //while (socket != null && !socket.isClosed()){}
+        } catch (Exception ex) {
+            System.out.println("Server not ready. \nMake sure server is listening before start connection!");
+            System.exit(0);
+        }
+    }
+}
---
+++ MySocket.java
@@ -0,0 +1,142 @@
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MySocket {
+    public Socket socket;
+    BufferedReader in;
+    PrintWriter out;
+    String nick;
+
+    public MySocket(String host, int port) {
+        try {
+            this.socket = new Socket(host, port);
+            //initialize text streams
+            this.initializeStreams();
+        } catch (Exception ex) {}
+    }
+
+    public MySocket(String host, int port, String nick) {
+        try {
+            this.socket = new Socket(host, port);
+            this.nick = nick;
+            //initialize text streams
+            this.initializeStreams();
+        } catch (Exception ex) {}
+    }
+
+    public MySocket(Socket s) {
+        this.socket = s;
+        //initialize text streams
+        this.initializeStreams();
+    }
+
+    private void initializeStreams() {
+        try {
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (Exception ex) {}
+    }
+
+    public Socket getSocket() {
+        return this.socket;
+    }
+
+    public String getNick() {
+        return this.nick;
+    }
+
+    public void setNick(String name) {
+        this.nick = name;
+    }    
+
+    public BufferedReader getInputStream() {
+        return this.in;
+    }
+
+    public PrintWriter getOutputStream() {
+        return this.out;
+    }
+
+    public int getLocalPort() {
+        return this.socket.getLocalPort();
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return this.socket.getRemoteSocketAddress();
+    }
+
+    public void flush() {
+        this.out.flush();
+    }
+
+    public int getPort() {
+        return this.socket.getPort();
+    }
+
+    public void close() {
+        try {
+            this.in.close();
+            this.out.close();
+            this.socket.close();
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    public boolean isClosed() {
+        return this.socket.isClosed();
+    }
+
+    public boolean ready() {
+        try {
+            return this.in.ready();
+        } catch (IOException ex) { ex.printStackTrace();
+            //System.out.println("Not ready to read: " + ex);
+        }
+        return false;
+    }
+
+    public String readLine() {
+        String s = null;
+        try {
+            s = this.in.readLine();
+        } catch (IOException ex) { ex.printStackTrace(); }
+
+        return s;
+    }
+
+    public void writeLine(String s) {
+        this.out.write(s);
+    }
+
+    public void println(String s) {
+        this.out.println(s);
+    }
+
+    public void sendNick() {
+        //this.writeLine(this.nick);
+        this.out.println(this.nick);
+        System.out.println("NICK SENT: " + this.nick);
+    }
+
+    public String receiveNick() {
+        return this.readLine();
+    }
+
+    public String setNick() {
+        this.nick = this.readLine();
+        return this.nick;
+    }
+
+    public void closeIn(){
+        try{
+            this.in.close();
+        }catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    public void closeOut(){
+        try{
+            this.out.close();
+        }catch (Exception ex) { ex.printStackTrace(); }
+    }
+}
