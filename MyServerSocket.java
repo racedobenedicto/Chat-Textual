ef6c16115494aac4975ef0f8b8ef7438f90d80ad
racedobenedicto <74828495+racedobenedicto@users.noreply.github.com>
2021-06-08 19:41:05 +0200

    Update MyServerSocket.java

--- MyServerSocket.java
+++ MyServerSocket.java
@@ -11,26 +11,6 @@ public class MyServerSocket {
         } catch (Exception ex) {}   
     }
 
-    public MyServerSocket(int port, int backlog) {
-        try {
-            this.serverSocket = new ServerSocket(port, backlog);
-        } catch (Exception ex) {}   
-    }
-    
-    public MyServerSocket(ServerSocket ss) {
-        try {
-            this.serverSocket = ss;
-        } catch (Exception ex) {}   
-    }
-
-    public void bind(SocketAddress endpoint) {
-        try{
-            this.serverSocket.bind(endpoint);
-        } catch (IOException ex) { ex.printStackTrace(); }
-    }
-    //bind con backlog
-
-    //accept que retorna Socket
     public MySocket accept() {
         MySocket socket = null;
         try {
@@ -45,19 +25,4 @@ public class MyServerSocket {
 		this.serverSocket.close();
 	} catch (IOException ex) { ex.printStackTrace(); }		
     }	
-	
-	public void setReceiveBufferSize(int size) {
-		try {
-		    this.serverSocket.setReceiveBufferSize(size);
-		} catch (IOException ex) { ex.printStackTrace(); }
-	}
-	
-	public int getReceiveBufferSize() {
-		int bufferSize = 0;
-		try {
-		    bufferSize = this.serverSocket.getReceiveBufferSize();
-		} catch (IOException ex) { ex.printStackTrace(); }
-
-		return bufferSize;
-	}
 }
